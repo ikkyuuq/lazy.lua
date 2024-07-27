48be2c5cb9ecc41df68d8b87ee6c153d2ed37dc5
ikkyuuq <kittipongprasompong@gmail.com>
2024-07-28 03:03:35 +0700

    fix: not enable when open buffer

--- lua/plugins/discipline.lua
+++ lua/plugins/discipline.lua
@@ -1,6 +1,6 @@
 return {
 	"m4xshen/hardtime.nvim",
-	command = "Hardtime",
+	command = "Hardtime enable",
 	event = "BufEnter",
 	dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
 	opts = {},
